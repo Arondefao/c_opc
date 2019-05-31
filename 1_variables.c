82a6fc3b482e6a89e7d04ea9a4eca89863730028
@doglobe <doglobegerard@gmail.com>
2019-05-31 14:02:11 +0200

    intial commit

---
+++ 1_variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>                                                                                                                                                                                                                                                                                                                                            
+int main (void){
+printf("\n");
+const double COEF_VIE =0.1;
+double nombreDeVie = 5, niveau = 2;
+float test;
+printf("saisir un flat:\n");
+scanf("%f",&test);
+printf("la valeur du float saisie est =%f \n",test);
+return EXIT_SUCCESS;
+}                                                                                                                                                                                                          
+                                                      
+                                                                                   
